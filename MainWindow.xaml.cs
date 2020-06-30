a7daabfa11bfcee61063cce13fb9cd3b5673b42d
GoAnyway <49254816+GoAnyway@users.noreply.github.com>
2020-06-30 20:04:04 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Phonebook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            addButton.Tag = Action.Add;
+            editButton.Tag = Action.Edit;
+            deleteButton.Tag = Action.Delete;
+        }
+
+        private void ActionsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Action action = (Action)((Button)sender).Tag;
+
+            switch (action)
+            {
+                case Action.Add:
+                    personesList.Items.Add(new Person { Name = "555", Surname = "666", Nickname = "rad", PhoneNumber = "900001" });
+                    break;
+
+                case Action.Edit:
+                    break;
+
+                case Action.Delete:
+                    break;
+            }
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Phonebook
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Nickname { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
