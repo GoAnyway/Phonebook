bb1dfc0862290a7ffa50060d58717db2b4ead66b
GoAnyway <49254816+GoAnyway@users.noreply.github.com>
2020-07-02 05:22:37 +0300

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,9 +8,11 @@ namespace Phonebook
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainViewModel MainViewModel { get; set; } = new MainViewModel();
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = MainViewModel;
 
             addButton.Tag = Action.Add;
             editButton.Tag = Action.Edit;
@@ -24,14 +26,14 @@ namespace Phonebook
             switch (action)
             {
                 case Action.Add:
-                    personesList.Items.Add(new Person { Name = "555", Surname = "666", Nickname = "rad", PhoneNumber = "900001" });
+                    //personesList.Items.Add(new Person { Name = "555", Surname = "666", Nickname = "rad", PhoneNumber = "900001" });
                     break;
 
                 case Action.Edit:
                     break;
 
                 case Action.Delete:
-                    personesList.Items.Remove(personesList.SelectedItem);
+                    //personesList.Items.Remove(personesList.SelectedItem);
                     break;
             }
         }
