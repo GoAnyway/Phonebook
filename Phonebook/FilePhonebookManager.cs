b69bb66be60ac5f25b8c983a750657e7b014f281
GoAnyway <49254816+GoAnyway@users.noreply.github.com>
2020-07-10 18:33:06 +0300

    Add files via upload

--- Phonebook/FilePhonebookManager.cs
+++ Phonebook/FilePhonebookManager.cs
@@ -8,14 +8,16 @@ namespace Phonebook
         public IEnumerable<PersonViewModel> Get()
         {
             var result = new List<PersonViewModel>();
+
             using (AppContext DB = new AppContext())
             {
-                var countries = DB.Persons;
-                foreach (Person p in countries)
+                var persons = DB.Persons;
+                foreach (Person p in persons)
                 {
                     result.Add(new PersonViewModel { Name = p.Name, Surname = p.Surname, Nickname = p.Nickname, PhoneNumber = p.PhoneNumber });
                 }
             }
+
             return result;
         }
     }
